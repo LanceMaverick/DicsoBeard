9e1d08e40c6a61793a241c306284577164debe24
LanceMaverick <pnoel6@gmail.com>
2018-10-16 00:34:55 +0000

    added csv export functions

--- beards/members/__init__.py
+++ beards/members/__init__.py
@@ -1,11 +1,17 @@
-from discobeard.beards import Plugin
+import csv
 from time import sleep
+import numpy as np
+import pandas as pd
+from discobeard.beards import Plugin
 
 class Members(Plugin):
     
     def register_commands(self):
        self.commands = [
-               ('members', self.get_member_list)]
+               ('members', self.get_member_list),
+               ('membermatrix', self.get_member_matrix),
+               ('membertable', self.get_member_table)
+               ]
 
     async def get_member_list(self, client, message, args):
         members = message.server.members
@@ -16,10 +22,45 @@ class Members(Plugin):
             names = u', '.join([m.name for m in members])
         
         replies = self.msg_split(names, delim=', ')
-        #for r in replies:
-        #    await client.send_message(message.channel, r.encode('utf-8'))
 
-#        self.relay_message('\n'.join(msg_list))
+    async def get_member_matrix(self, client, message, args):
+        roles = message.server.roles
+        members = message.server.members
+        
+        data = [['']+[r.name for r in roles]]
+        for i, m in enumerate(members):
+            data.append([m.name.encode()])
+            for r in roles:
+                if r in m.roles:
+
+                    data[i+1].append(r.name.replace('=', '', 2))
+                else:
+                    data[i+1].append('')
+
+        with open("x51.csv", "w") as outfile:
+            writer = csv.writer(outfile)
+            header = [x.replace('=', '', 2) for x in data.pop(0)]
+            writer.writerow(header)
+            writer.writerows(data)
+        with open("x51.csv", "rb") as sendfile:
+            await client.send_file(message.channel, sendfile, content="X51 roster:")
+
+    async def get_member_table(self, client, message, args):
+        roles = message.server.roles
+        members = message.server.members
+        
+        data = []
+        for i, m in enumerate(members):
+            data.append([m.name.encode()])
+            for r in roles:
+                if r in m.roles:
+                    data[i].append(r.name.replace("=", "", 2))
+        
+        with open("x51.csv", "w") as outfile:
+            writer = csv.writer(outfile)
+            writer.writerows(data)
+        with open("x51.csv", "rb") as sendfile:
+            await client.send_file(message.channel, sendfile, content="X51 roster:")
 
     #factor out into helpers (make a generator?)
     def msg_split(self, msg, delim = ' ', max_length=200):
@@ -34,6 +75,8 @@ class Members(Plugin):
 
         lines.append(current)
         return lines
+
+
         
         
         
